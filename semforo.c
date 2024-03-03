f7f86895f1bfa638476ca8f73d1edaa32b76fcbd
Spartoons <113599441+Spartoons@users.noreply.github.com>
2024-03-03 22:18:39 +0100

    Create semforo.c

---
+++ semforo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main (void)
+{
+	int r = 10, y = 13, g = 16;
+
+	wiringPiSetup();
+
+
+  while True{
+    digitalWrite (g, HIGH);
+    printf();
+    digitalWrite (r, LOW);
+    printf();
+    digitalWrite (y, LOW);
+		delay (7000);
+    digitalWrite (y, HIGH);
+    printf();
+    digitalWrite (r, LOW);
+    printf();
+    digitalWrite (g, LOW);
+		delay (4000);
+    digitalWrite (r, HIGH);
+    printf();
+    digitalWrite (g, LOW);
+    printf();
+    digitalWrite (y, LOW);
+		delay (6000);
+
+	return 0;
+}
