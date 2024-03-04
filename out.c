d83e4510ffc7262f106fa401cac6d60236f035e3
Spartoons <113599441+Spartoons@users.noreply.github.com>
2024-03-04 21:12:31 +0100

    Update out.c

--- out.c
+++ out.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main (void)
+{
+	int i = 0;
+
+	wiringPiSetup();
+
+	for (i = 0; i < 21; i++)
+  {
+		pinMode (i, OUTPUT) ;
+  }
+
+	for (i = 0; i < 21; i++)
+  {
+    digitalWrite (i, LOW);
+	}
+
+	return 0;
+}
